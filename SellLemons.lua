1871db280d2bc6e410102cd56f299ddb1c342d00
xcronz22 <113306353+xcronz22@users.noreply.github.com>
2026-06-01 06:31:25 +0700

    Update SellLemons.lua

--- SellLemons.lua
+++ SellLemons.lua
@@ -283,7 +283,7 @@ if MyTycoon and Toggles.AutoUpgrade then
         if remote and remote.Parent then
             task.spawn(function()
                 pcall(function()
-                    remote:InvokeServer(5)
+                    remote:InvokeServer(1)
                 end)
             end)
         else
