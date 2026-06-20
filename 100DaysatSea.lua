f3ee2d2f7ccbcf309bc71084bd1901c1266533b8
xcronz22 <113306353+xcronz22@users.noreply.github.com>
2026-06-21 04:35:27 +0700

    Update 100DaysatSea.lua

--- 100DaysatSea.lua
+++ 100DaysatSea.lua
@@ -487,7 +487,7 @@ Win:AddToggle("Mulai Auto Attack", false, function(state)
                         end
                     end
                 end
-                task.wait(0.4) 
+                task.wait(0.1) 
             end
         end)
     end
