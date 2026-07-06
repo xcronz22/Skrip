af53ede9466893499b9d2ddc7fa10d2f07b4ac99
xcronz22 <113306353+xcronz22@users.noreply.github.com>
2026-07-07 03:37:32 +0700

    Update AnimalHospitalA.lua

--- AnimalHospitalA.lua
+++ AnimalHospitalA.lua
@@ -290,7 +290,7 @@ task.spawn(function()
                         if mainCheckIn then
                             while activeNPC and activeNPC.Parent and AutoCheckInEnabled do
                                 local root = activeNPC:FindFirstChild("HumanoidRootPart") or activeNPC.PrimaryPart
-                                if not root or (root.Position - bellPos).Magnitude > 3 then break end
+                                if not root or (root.Position - bellPos).Magnitude > 5 then break end
 
                                 pcall(function()
                                     firePromptIn(activeDesk:FindFirstChild("Form")); task.wait(0.1)
